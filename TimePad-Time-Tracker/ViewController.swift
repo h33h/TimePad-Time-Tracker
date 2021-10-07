1fe66b1445246f91d4bb7820ae37abe1e4837758
h33h___ <beolki102@gmail.com>
2021-10-07 21:49:03 +0300

    Initial commit (start point)

---
+++ TimePad-Time-Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimePad-Time-Tracker
+//
+//  Created by xxx on 7.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

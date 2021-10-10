27f4b6fa327147a63c099710b97fc0abcb0f1e46
Eugene Fedotov <beolki102@gmail.com>
2021-10-10 14:28:47 +0300

    Add Time and Color Structures in Model

---
+++ TimePad-Time-Tracker/Model/Structures.swift
@@ -0,0 +1,21 @@
+//
+//  Structures.swift
+//  TimePad-Time-Tracker
+//
+//  Created by XXX on 10.10.21.
+//
+
+import Foundation
+
+public struct Color {
+    var red: Float
+    var green: Float
+    var blue: Float
+    var alpha: Float
+}
+
+public struct Time {
+    var hours: Int
+    var minutes: Int
+    var seconds: Int
+}

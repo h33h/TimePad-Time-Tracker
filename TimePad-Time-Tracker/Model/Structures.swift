961265637f4922820761fe384b708e89b31fe220
h33h <beolki102@gmail.com>
2021-10-14 02:39:37 +0300

    Update Structures

--- TimePad-Time-Tracker/Model/Structures.swift
+++ TimePad-Time-Tracker/Model/Structures.swift
@@ -6,16 +6,26 @@
 //
 
 import Foundation
+import UIKit
 
 public struct Color {
     var red: Float
     var green: Float
     var blue: Float
     var alpha: Float
+    func convertColor() -> UIColor {
+        UIColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue), alpha: CGFloat(self.alpha))
+    }
 }
 
 public struct Time {
     var hours: Int
     var minutes: Int
     var seconds: Int
+    func toSeconds() -> Int {
+        (self.hours * 60 * 60) + (minutes * 60) + seconds
+    }
+    func toString() -> String {
+        "\(String(format: "%.2d", hours)):\(String(format: "%.2d", minutes)):\(String(format: "%.2d", seconds))"
+    }
 }
